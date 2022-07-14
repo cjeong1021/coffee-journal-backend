6474293f5bcce3d574f958217a49db83c9f9ab12
Christopher <cjeong1021@gmail.com>
2022-07-14 17:17:12 -0400

    finish authentication

--- coffee_journal/urls.py
+++ coffee_journal/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path("users/", views.UserList.as_view(), name="user_list"),
     path("users/<int:pk>", views.UserDetail.as_view(), name="user_detail"),
     path("coffees/", views.CoffeeList.as_view(), name="coffee_list"),
     path("coffees/<int:pk>", views.CoffeeDetail.as_view(), name="coffee_detail"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
