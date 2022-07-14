bd74b7fd677f574970da3621db749a5f606ce04d
Christopher <cjeong1021@gmail.com>
2022-07-14 13:07:05 -0400

    update

--- coffee_journal_django/settings.py
+++ coffee_journal_django/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "coffee_journal",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -134,3 +137,5 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
