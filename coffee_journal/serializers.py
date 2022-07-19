bd9d0a31404754629761b9ad73871fb6bab5399b
Christopher <cjeong1021@gmail.com>
2022-07-19 10:56:01 -0400

    add user authentication to coffees

--- coffee_journal/serializers.py
+++ coffee_journal/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Profile, Coffee
 
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("id", "name", "fav_roast", "brew_method")
+        fields = ("user", "id", "name", "fav_roast", "brew_method")
 
 
-class CoffeeSerializer(serializers.HyperlinkedModelSerializer):
+class CoffeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coffee
-        fields = ("id", "name", "roast", "origin", "notes", "brew_method")
+        fields = ("user", "id", "name", "roast", "origin", "notes", "brew_method")
--- coffee_journal/views.py
+++ coffee_journal/views.py
@@ -1,7 +1,9 @@
 from queue import PriorityQueue
+from urllib import request
 from rest_framework import generics
 from .serializers import ProfileSerializer, CoffeeSerializer
 from .models import Profile, Coffee
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from rest_framework.permissions import (
     BasePermission,
     SAFE_METHODS,
@@ -20,33 +22,44 @@ class WritePermission(BasePermission):
         return obj.user == request.user
 
 
-class ProfileList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Profile.objects.all()
+class ProfileList(generics.ListCreateAPIView, WritePermission):
+    permission_classes = [WritePermission]
     serializer_class = ProfileSerializer
 
     def get_queryset(self):
-        user = self.request.user
+        user = self.request.user.id
         return Profile.objects.filter(user=user)
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView, WritePermission):
     permission_classes = [WritePermission]
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+    def get_queryset(self):
+        user = self.request.user.id
+        return Profile.objects.filter(user=user)
 
-class CoffeeList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Coffee.objects.all()
+
+class CoffeeList(generics.ListCreateAPIView, WritePermission):
+    permission_classes = [IsAuthenticated]
     serializer_class = CoffeeSerializer
 
     def get_queryset(self):
-        user = self.request.user
+        user = self.request.user.id
         return Coffee.objects.filter(user=user)
+
+    def create(self, request, **kwargs):
+        serializer = CoffeeSerializer(data=self.request.data)
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return HttpResponse(request.user)
+        return HttpResponse(request.data)
 
 
 class CoffeeDetail(generics.RetrieveUpdateDestroyAPIView, WritePermission):
     permission_classes = [WritePermission]
-    queryset = Coffee.objects.all()
     serializer_class = CoffeeSerializer
+
+    def get_queryset(self):
+        user = self.request.user.id
+        return Coffee.objects.filter(user=user)
